9d070458d504d491155f24710f204088aa2f2a8c
iTRobert <quangnamhd9x@gmail.com>
2020-09-06 21:50:52 +0700

    update

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="first.php" method="post">
+    <p>Name: <br> <input type="text" name="Name:" /></p>
+    <p>Email: <br> <input type="text" name="Email" /></p>
+    <p>Phone:: <br> <input type="text" name="Email" /></p>
+    <p><input type="submit" name="submit" value="Register" /></p>
+</form>
+</body>
+</html>
+<?php
